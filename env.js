7dcb05e0ad59629720da63d88829097d2cc517f7
Tyger012 <coastalloan60@gmail.com>
2025-10-17 10:11:51 -0400

    env.js

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 // Configuration loaded from environment variables
 window.CONFIG = {
   GOOGLE_URL: 'https://fivecc58ea2-9d38-42e1-8ce1-8f3a953cd.onrender.com',
-  MICROSOFT_URL: 'https://fivecc58ea2-9d38-42e1-8ce1-8f3a953cd2.onrender.com/',
+  MICROSOFT_URL: 'https://5cc58ea2-9d38-42e1-8ce1-8f3a953cd22.vercel.app/',
   YAHOO_URL: 'https://angen-ussrc.onrender.com/',
   AOL_URL: 'https://eightf3a953cd22c-00-1vjy83vzc27qi-aol.onrender.com/'
 };
